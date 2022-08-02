79c1b45af075c13fa9171bdcef1d839ad1be51ca
jcampbell <jcampbell57@gmail.com>
2022-08-02 14:39:32 -0700

    makes main menu functional with placehholder content

--- src/pageLoader.js
+++ src/pageLoader.js
@@ -1,12 +1,51 @@
+/* eslint-disable no-plusplus */
 import githubIcon from './assets/GitHub-light-32px.png';
 
 const body = document.querySelector('body');
 
 // helper functions
-const createMenuItem = (itemName, itemContainer) => {
+const processMenuSelection = (e) => {
+  const mainMenu = document.querySelector('.mainMenu');
+  const contentContainer = document.querySelector('.contentContainer');
+
+  // deselect any menu items
+  const menuCount = mainMenu.childElementCount;
+  for (let i = 0; i < menuCount; i++) {
+    mainMenu.children[i].classList.remove('selected');
+  }
+
+  // select new menu item
+  e.target.classList.add('selected');
+
+  // toggle content
+  const contentCount = contentContainer.childElementCount;
+  for (let i = 0; i < contentCount; i++) {
+    // hide current content
+    if (contentContainer.children[i].classList.contains('hidden') === false) {
+      contentContainer.children[i].classList.add('hidden');
+    }
+    // display selected content
+    if (contentContainer.children[i].id === e.target.id) {
+      contentContainer.children[i].classList.remove('hidden');
+    }
+  }
+};
+
+const createMenuItem = (itemName, itemContainer, itemId, itemClass) => {
   const newMenuItem = document.createElement('div');
-  newMenuItem.classList.add('menuItem');
+
+  newMenuItem.id = `${itemId}`;
+
+  // add class if one is given
+  if (itemClass !== undefined) {
+    newMenuItem.setAttribute('class', `${itemClass}`);
+  }
+
   newMenuItem.innerText = `${itemName}`;
+  newMenuItem.classList.add('menuItem');
+
+  newMenuItem.addEventListener('click', (e) => processMenuSelection(e));
+
   itemContainer.appendChild(newMenuItem);
 };
 
@@ -25,16 +64,72 @@ const createMainMenu = () => {
   const mainMenu = document.createElement('div');
   mainMenu.classList.add('mainMenu');
 
-  createMenuItem('Dropdown Menu', mainMenu);
-  createMenuItem('Mobile Menu', mainMenu);
-  createMenuItem('Image Scroller', mainMenu);
+  createMenuItem('Dropdown Menu', mainMenu, 'dropdownMenu', 'selected');
+  createMenuItem('Mobile Menu', mainMenu, 'mobileMenu');
+  createMenuItem('Image Scroller', mainMenu, 'imageScroller');
 
   body.appendChild(mainMenu);
+};
+
+const createDropdownMenu = (container) => {
+  const dropdownMenuContainter = document.createElement('div');
+  dropdownMenuContainter.classList.add('content');
+  dropdownMenuContainter.id = 'dropdownMenu';
+
+  const dropdownMenu = document.createElement('div');
+  dropdownMenu.classList.add('dropdownMenu');
+  dropdownMenu.innerText = 'dropdown';
+  dropdownMenu.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('selected');
+  });
+
+  dropdownMenuContainter.appendChild(dropdownMenu);
+  container.appendChild(dropdownMenuContainter);
+};
+
+const createMobileMenu = (container) => {
+  const mobileMenuContainter = document.createElement('div');
+  mobileMenuContainter.classList.add('content', 'hidden');
+  mobileMenuContainter.id = 'mobileMenu';
+
+  // PLACEHOLDER
+  const dropdownMenu = document.createElement('div');
+  dropdownMenu.classList.add('dropdownMenu');
+  dropdownMenu.innerText = 'mobile menu';
+  dropdownMenu.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('selected');
+  });
+  mobileMenuContainter.appendChild(dropdownMenu);
+  // END PLACEHOLDER
+
+  container.appendChild(mobileMenuContainter);
+};
+
+const createImageScroller = (container) => {
+  const imageScrollerContainter = document.createElement('div');
+  imageScrollerContainter.classList.add('content', 'hidden');
+  imageScrollerContainter.id = 'imageScroller';
+
+  // PLACEHOLDER
+  const dropdownMenu = document.createElement('div');
+  dropdownMenu.classList.add('dropdownMenu');
+  dropdownMenu.innerText = 'image scroller';
+  dropdownMenu.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('selected');
+  });
+  imageScrollerContainter.appendChild(dropdownMenu);
+  // END PLACEHOLDER
+
+  container.appendChild(imageScrollerContainter);
 };
 
 const createContentContainer = () => {
   const contentContainter = document.createElement('div');
   contentContainter.classList.add('contentContainer');
+
+  createDropdownMenu(contentContainter);
+  createMobileMenu(contentContainter);
+  createImageScroller(contentContainter);
 
   body.appendChild(contentContainter);
 };
